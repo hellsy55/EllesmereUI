24250a7634effc8db3dfc0346954cabe8be33b56
manaste <manaste@mailbox.org>
2026-08-28 13:15:44 +0200

    fix(raidframes): respect status text setting in preview mode

--- EllesmereUIRaidFrames/EllesmereUIRaidFrames.lua
+++ EllesmereUIRaidFrames/EllesmereUIRaidFrames.lua
@@ -13587,7 +13587,10 @@ local function ApplyPreviewData(f, index)
         else
             f._statusText:SetPoint("CENTER", stHost, "CENTER", stOX, stOY)
         end
-        if isRezCorpse then
+        if stPos == "none" then
+            -- Status text display is turned off
+            f._statusText:Hide()
+        elseif isRezCorpse then
             -- Being resurrected: the rez icon takes this spot, so no DEAD text.
             f._statusText:Hide()
         elseif isDead then
@@ -13600,6 +13603,7 @@ local function ApplyPreviewData(f, index)
             f._statusText:SetText(EllesmereUI.L("AFK"))
             f._statusText:Show()
         else
+            -- No status to show
             f._statusText:Hide()
         end
     end
